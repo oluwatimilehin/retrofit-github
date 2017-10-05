8406537ac2532a8d79bc921e9e1b11a83f40d99b
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-05 15:22:11 +0100

    Design layout for credentials dialog

---
+++ app/src/main/java/com/example/oluwatimilehin/retrofitgithub/models/CredentialDialog.java
@@ -0,0 +1,11 @@
+package com.example.oluwatimilehin.retrofitgithub.models;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Oluwatimilehin on 05/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+
+public class CredentialDialog extends DialogFragment {
+}
