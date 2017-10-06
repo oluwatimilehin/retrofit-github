b6e2dfe5f5bdb5f28b02a47623ed41637e5eb001
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-06 21:34:18 +0100

    Instantiate Githubservice object

--- app/src/main/java/com/example/oluwatimilehin/retrofitgithub/MainActivity.java
+++ app/src/main/java/com/example/oluwatimilehin/retrofitgithub/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.oluwatimilehin.retrofitgithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.example.oluwatimilehin.retrofitgithub.models.CredentialDialog;
+import com.example.oluwatimilehin.retrofitgithub.models.GithubRepo;
+import com.example.oluwatimilehin.retrofitgithub.models.GithubRepoDeserializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CredentialDialog.CredentialsDialogListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    String userName;
+    String password;
+
+    GithubService githubService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,45 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        createGithubApi();
+    }
+
+    private void createGithubApi(){
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .registerTypeAdapter(GithubRepo.class, new GithubRepoDeserializer())
+                .create();
+
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain
+                        .request()
+                        .newBuilder()
+                        .addHeader("Authorization", Credentials.basic(userName, password))
+                        .build();
+                return  chain.proceed(request);
+            }
+        };
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .addInterceptor(interceptor);
+        OkHttpClient client = builder.build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+        githubService = retrofit.create(GithubService.class);
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(String username, String password) {
+        this.userName = username;
+        this.password = password;
     }
 }
