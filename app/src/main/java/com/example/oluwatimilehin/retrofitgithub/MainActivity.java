392112b5e60d52c86c7288e04207d0ab79c8abd6
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-06 22:00:37 +0100

    Set up views

--- app/src/main/java/com/example/oluwatimilehin/retrofitgithub/MainActivity.java
+++ app/src/main/java/com/example/oluwatimilehin/retrofitgithub/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.oluwatimilehin.retrofitgithub;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.oluwatimilehin.retrofitgithub.models.CredentialDialog;
 import com.example.oluwatimilehin.retrofitgithub.models.GithubRepo;
@@ -10,15 +13,12 @@ import com.example.oluwatimilehin.retrofitgithub.models.GithubRepoDeserializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import okhttp3.Credentials;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -26,6 +26,16 @@ public class MainActivity extends AppCompatActivity implements CredentialDialog.
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.reposSpinner)
+    Spinner reposSpinner;
+    @BindView(R.id.issuesSpinner)
+    Spinner issuesSpinner;
+    @BindView(R.id.commentField)
+    EditText commentField;
+    @BindView(R.id.loadReposButton)
+    Button loadReposButton;
+    @BindView(R.id.postCommentButton)
+    Button postCommentButton;
 
     String userName;
     String password;
@@ -44,22 +54,19 @@ public class MainActivity extends AppCompatActivity implements CredentialDialog.
         createGithubApi();
     }
 
-    private void createGithubApi(){
+    private void createGithubApi() {
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
                 .registerTypeAdapter(GithubRepo.class, new GithubRepoDeserializer())
                 .create();
 
-        Interceptor interceptor = new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request request = chain
-                        .request()
-                        .newBuilder()
-                        .addHeader("Authorization", Credentials.basic(userName, password))
-                        .build();
-                return  chain.proceed(request);
-            }
+        Interceptor interceptor = chain -> {
+            Request request = chain
+                    .request()
+                    .newBuilder()
+                    .addHeader("Authorization", Credentials.basic(userName, password))
+                    .build();
+            return chain.proceed(request);
         };
 
         OkHttpClient.Builder builder = new OkHttpClient.Builder()
