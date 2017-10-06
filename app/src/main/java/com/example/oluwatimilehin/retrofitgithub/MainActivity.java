e286e77eb41fb070f8c223f7f0260c850eeed083
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-06 23:22:04 +0100

    Implement observable to get repos

--- app/src/main/java/com/example/oluwatimilehin/retrofitgithub/MainActivity.java
+++ app/src/main/java/com/example/oluwatimilehin/retrofitgithub/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.oluwatimilehin.retrofitgithub;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -13,8 +17,14 @@ import com.example.oluwatimilehin.retrofitgithub.models.GithubRepoDeserializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.Credentials;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -39,8 +49,8 @@ public class MainActivity extends AppCompatActivity implements CredentialDialog.
 
     String userName;
     String password;
-
     GithubService githubService;
+    CompositeDisposable compositeDisposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +60,9 @@ public class MainActivity extends AppCompatActivity implements CredentialDialog.
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        reposSpinner.setEnabled(false);
+        issuesSpinner.setEnabled(false);
 
         createGithubApi();
     }
@@ -87,5 +100,84 @@ public class MainActivity extends AppCompatActivity implements CredentialDialog.
     public void onDialogPositiveClick(String username, String password) {
         this.userName = username;
         this.password = password;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.credentials:
+                showCredentialsDialog();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showCredentialsDialog() {
+        CredentialDialog dialog = new CredentialDialog();
+        Bundle bundle = new Bundle();
+        bundle.putString("username", userName);
+        bundle.putString("password", password);
+        dialog.setArguments(bundle);
+
+        dialog.show(getSupportFragmentManager(), dialog.getClass().getSimpleName());
+        loadReposButton.setEnabled(true);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.loadReposButton:
+                compositeDisposables.add(githubService.getRepos()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                (List<GithubRepo> value) -> {
+                                    if (!value.isEmpty()) {
+                                        ArrayAdapter<GithubRepo> adapter = new ArrayAdapter<GithubRepo>
+                                                (MainActivity.this,
+                                                        android.R.layout.simple_spinner_dropdown_item, value);
+                                        reposSpinner.setAdapter(adapter);
+                                        reposSpinner.setEnabled(true);
+                                    } else {
+                                        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                                                MainActivity.this, android.R.layout
+                                                .simple_spinner_dropdown_item, new String[]{"No " +
+                                                "data"});
+                                        reposSpinner.setAdapter(adapter);
+                                        reposSpinner.setEnabled(false);
+                                    }
+                                },
+                                (Throwable e) -> e.printStackTrace()));
+                break;
+        }
+    }
+
+    private DisposableSingleObserver<List<GithubRepo>> getReposObserver() {
+
+
+        return new DisposableSingleObserver<List<GithubRepo>>() {
+            @Override
+            public void onSuccess(List<GithubRepo> value) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposables.dispose();
     }
 }
